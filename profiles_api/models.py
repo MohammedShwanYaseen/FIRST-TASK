5fd54ddb2101e7f8803e6eb0e06978248ee9aed1
Mohammed Yaseen <mohammedshwan76@gmail.com>
2022-02-14 15:59:24 +0200

    admin

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -52,3 +52,7 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     def __str__(self):
         """Return string representation of user"""
         return self.email
+
+    def __str__(self):
+        """Return string representation of user"""
+        return self.email
